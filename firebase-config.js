303d9006dbf6bd45b279f742a3b59be400849c08
HagoX7 <151998806+HagoX7@users.noreply.github.com>
2025-06-27 10:40:15 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBCTMyjwARc9WLPLyr6NuVSeAI7jlxUR9Q",
-  authDomain: "openchat-165b4.firebaseapp.com",
-  databaseURL: "https://openchat-165b4-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "openchat-165b4",
-  storageBucket: "openchat-165b4.appspot.com",
-  messagingSenderId: "810981595067",
-  appId: "1:810981595067:web:8b253f000c7154469bd69e",
-  measurementId: "G-HZNDGCPKC8"
+  apiKey: "AIzaSyAQtQ0qbdaREikcVxVOHMDQOggwKhDG8eI",
+  authDomain: "signals-net.firebaseapp.com",
+  projectId: "signals-net",
+  storageBucket: "signals-net.firebasestorage.app",
+  messagingSenderId: "615864152366",
+  appId: "1:615864152366:web:fedeb112101fe7d0148abf",
+  measurementId: "G-HW9EQ9E901"
 };
