e64e5010c2b204fb90cf1dbb2caef0afe1b04f50
HagoX7 <151998806+HagoX7@users.noreply.github.com>
2025-06-27 09:50:32 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBCTMyjwARc9WLPLyr6NuVSeAI7jlxUR9Q",
+  authDomain: "openchat-165b4.firebaseapp.com",
+  databaseURL: "https://openchat-165b4-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "openchat-165b4",
+  storageBucket: "openchat-165b4.appspot.com",
+  messagingSenderId: "810981595067",
+  appId: "1:810981595067:web:8b253f000c7154469bd69e",
+  measurementId: "G-HZNDGCPKC8"
+};
