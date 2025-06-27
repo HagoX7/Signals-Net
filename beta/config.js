8e573ef96b57c3b421b8fb2b18f769a9d4001fd0
HagoX7 <151998806+HagoX7@users.noreply.github.com>
2025-06-27 10:41:21 +0530

    Update config.js

--- beta/config.js
+++ beta/config.js
@@ -1,12 +1,12 @@
-// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBPF1VE82Y3VkZe6IibjqKxBC-XHjM_Wco",
-    authDomain: "chat-2024-ff149.firebaseapp.com",
-    databaseURL: "https://chat-2024-ff149-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "chat-2024-ff149",
-    storageBucket: "chat-2024-ff149.appspot.com",
-    messagingSenderId: "146349109253",
-    appId: "1:146349109253:android:e593afbf0584762519ac6c"
+  apiKey: "AIzaSyAQtQ0qbdaREikcVxVOHMDQOggwKhDG8eI",
+  authDomain: "signals-net.firebaseapp.com",
+  projectId: "signals-net",
+  storageBucket: "signals-net.firebasestorage.app",
+  messagingSenderId: "615864152366",
+  appId: "1:615864152366:web:fedeb112101fe7d0148abf",
+  measurementId: "G-HW9EQ9E901"
 };
 
 // Initialize Firebase
